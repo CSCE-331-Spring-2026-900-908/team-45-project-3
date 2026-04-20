d5171a065b9042a2020a42b456e69341f09d5ae9
Peter Shenouda <peter.shenouda05@gmail.com>
2026-04-20 13:04:03 -0500

    fixing

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function createApp() {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "/auth/google/callback"
+      callbackURL: "/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
